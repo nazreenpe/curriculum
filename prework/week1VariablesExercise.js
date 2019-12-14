e4f6e68ea0aaf883aa886efa04eb9e195c011a5b
Nasreen Habeeb Ur Rahiman <35543664+nazreenpe@users.noreply.github.com>
2019-12-14 11:15:46 -0800

    Updated the answers of prework

--- prework/week1VariablesExercise.js
+++ prework/week1VariablesExercise.js
@@ -13,19 +13,20 @@
 
 // Exercise 1. Simply declare a variable named 'emptyVariable'
 
-
+let emptyVariable;    
 
 // Exercise 2. Declare a variable called 'myName'
 
-
+let myName;
 
 // Exercise 3. Assign your name as the value for 'myName'
 
-
+myName = "Nasreen";
 
 // Exercise 4. Declare a variable called dreamDestination and give it the value
 // of what your dream vacation destination is.
 
+let dreamDestination = "Istanbul";
 
 
 // Exercise 5. Make the following variables and assignments
@@ -33,12 +34,14 @@
 // - a variable called num2 with a value of greater than or equal to 1 and less
 // than or equal to 10
 
-
+let num1 = 2;
+let num2 = 1;   
 
 // Exercise 6. Write a comment below explaining the difference between the rules
 // for num1 and num2 from Exercise 5. What numbers would be valid values for 
 // num2 but not num1?
-//
+//num1 can have values from 2 to 9. 
+// num2 can have values from 1 to 10, including 1 and 10. But num1 does not include 1 or 10.
 
 
 // Exercise 7. Now we will try some addition.
@@ -46,13 +49,15 @@
 // b. Make a new variable called 'theSum', and use 'num1' and
 // 'num2' to assign its value using the "+" operator.
 
-
+let num1 = 4;
+let num2 = 6;
+let theSum = num1 + num2;
 
 // Exercise 8. Now we will try some multiplication.
 // Make a new variable called 'theProduct', multiply num1 and num2 and assign
 // the resulting value to 'theProduct'.
 
-
+let theProduct = num1 * num2;
 
 // Exercise 9.
 // Make a new variable called 'statement' and using 'myName' and
@@ -60,6 +65,7 @@
 // value of statement to be:
 // "Hi, my name is <your name>, and I can't wait to visit <your destination>!"
 
+let statement = "Hi, my name is " + myName + ", and I can't wait to visit " + dreamDestination + "!"
 
 
 // *NOTE* For the following exercises, put your answers in the line below
@@ -71,17 +77,17 @@
 // Exercise 10. What is the data type of the value stored in 'dreamDestination',
 // place your answer in the quotes below:
 
-var myAnswerForExercise10 = "";
+var myAnswerForExercise10 = "string";
 
 // Exercise 11. What is the data type of the value stored in 'product',
 // place your answer in the quotes below:
 
-var myAnswerForExercise11 = "";
+var myAnswerForExercise11 = "number";
 
 // Exercise 12. What do you think the value of emptyVariable is?
 // Place your answer below:
 
-var myAnswerForExercise12 = ;
+var myAnswerForExercise12 = "undefined" ;
 
 // Exercise 13. If
 var A = "R";
@@ -90,36 +96,36 @@ var C = 4;
 var D = "D";
 
 // What is the value of B + C
-var myAnswerForExercise13 = ;
+var myAnswerForExercise13 = 5;
 
 // Exercise 14 What is the value of
 // A + "n" + D
-var myAnswerForExercise14 = ;
+var myAnswerForExercise14 = "RnD;
 
 // Exercise 15 What is the value of
 // A + B * 2 + D + C / 2:
-var myAnswerForExercise15 = ;
+var myAnswerForExercise15 = "R2D2";
 
 // Exercise 16 What is the value of A * B
-var myAnswerForExercise16 = ;
+var myAnswerForExercise16 = NaN;
 
 // Exercise 17 What is the value of true || false
-var myAnswerForExercise17 = ;
+var myAnswerForExercise17 = true;
 
 // Exercise 18 What is the value of 1 === "1"
-var myAnswerForExercise18 = ;
+var myAnswerForExercise18 = false;
 
 // Exercise 19 What is the value of 1 == "1"
-var myAnswerForExercise19 = ;
+var myAnswerForExercise19 = true;
 
 // Exercise 20 What is the value of 1 < 1
-var myAnswerForExercise20 = ;
+var myAnswerForExercise20 = false;
 
 // Exercise 21 What is the value of 1 < 2 < 3
-var myAnswerForExercise21 = ;
+var myAnswerForExercise21 = true;
 
 // Exercise 22 What is the value of 2 < 1 < 3
-var myAnswerForExercise22 = ;
+var myAnswerForExercise22 = true;
 
 // Congrats, you made it to the end!
 // Did you find this easy or hard? If you used references, which ones helped you? 
